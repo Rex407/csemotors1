4e155c5e577efe63388333f3ce880614803e4f94
Rex407 <rexkapolom@outlook.com>
2026-01-14 23:44:14 +0200

    changes

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-
-// View Engine and Templates
-
+/* ***********************
+ * View Engines and Templates
+ *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -22,6 +22,12 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+
+
+// Index route
+app.get("/", function(req, res){
+res. render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
@@ -37,8 +43,3 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-// Index route
-app.get("/", function(req, res){
-res. render("index", {title: "Home"})
-
-})
